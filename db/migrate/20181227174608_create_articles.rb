ff10fb4f2b037245fa2946401e495c0be6e46926
Wais Shahbaz <wais.shahbaz@gmail.com>
2018-12-28 20:44:19 -0500

    fixed db migrate file

--- db/migrate/20181227174608_create_articles.rb
+++ db/migrate/20181227174608_create_articles.rb
@@ -3,7 +3,6 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.text :body
-      t.integer, 0 :view_count
 
       t.timestamps
     end
